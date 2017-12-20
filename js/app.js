110fa048b80d0a5b49e6f33bfa535e16eca9f99d
Michael Baxter <msb@Michaels-MBP.localdomain>
2017-12-20 10:56:15 -0600

    modified and reformatted

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
 
 
+
+for (i = 0; i <3;i++){
+
 // ___________Request Number One____________//
-$("<div>").attr("id","number-one-request").addClass("selection-container")
+$("<div>").attr("id","user-request").addClass("selection-container")
 .append($("<label>").html("1."))
 .append($("<div>").attr("id","poster-title-container")
 .append($("<img>").attr("src","http://starwarsblog.starwars.com/wp-content/uploads/2017/10/the-last-jedi-theatrical-blog.jpg"))
-.append($("<button>").html("Remove").click(function (){
+.append($("<img>").addClass("trash-can-image").attr("src","images/trashcan.png").click(function (){
    var answer = confirm("Are you sure you want to delete this request?");
       if (answer) {
-         $("#number-one-request").remove();
+         $("#user-request").remove();
       }else{
          return false;
       }
@@ -16,39 +19,10 @@ $("<div>").attr("id","number-one-request").addClass("selection-container")
 .append($("<h3>").html("Movie Title Here.")))
 
 .appendTo($("#movie-choice-container"))
+}
+$("#movie-choice-container").sortable()
 
-//_____________Request Number 2____________//
-$("<div>").attr("id","number-two-request").addClass("selection-container")
-.append($("<label>").html("2."))
-.append($("<div>").attr("id","poster-title-container")
-.append($("<img>").attr("src","http://starwarsblog.starwars.com/wp-content/uploads/2017/10/the-last-jedi-theatrical-blog.jpg"))
-.append($("<button>").html("Remove").click(function (){
-   var answer = confirm("Are you sure?");
-      if (answer) {
-         return true
-         ;
-      }else{
-         return false;
-      }
-}))
-.append($("<h3>").html("Movie Title Here.")))
-.appendTo($("#movie-choice-container"))
 
-//_______________Request Number 3______________//
-$("<div>").attr("id","number-three-request").addClass("selection-container")
-.append($("<label>").html("3."))
-.append($("<div>").attr("id","poster-title-container")
-.append($("<img>").attr("src","http://starwarsblog.starwars.com/wp-content/uploads/2017/10/the-last-jedi-theatrical-blog.jpg"))
-.append($("<button>").html("Remove").click(function (){
-   var answer = confirm("Are you sure?");
-      if (answer) {
-         return true;
-      }else{
-         return false;
-      }
-}))
-.append($("<h3>").html("Movie Title Here")))
-.appendTo($("#movie-choice-container"))
 
 
 
@@ -75,7 +49,3 @@ $( function() {
       runEffect();
     });
   })
-
-
-
-movieChoiceContainer.sortable()
