cd71f698f4a466a0e7a81e32d43568f8e94b1eb8
Daniel Dyer <danielbdyer@gmail.com>
2017-12-21 10:45:29 -0600

    Improvement to interface, basic request functionality active

--- main-page/js/main.js
+++ main-page/js/main.js
@@ -2,10 +2,22 @@ let apikey = "6fe893b2";
 let searchterm = "star";
 let url = "http://www.omdbapi.com/?s=" + searchterm + "&apikey=" + apikey;
 let resultsplacement = $(".results-container");
-let $bio = $(".bio");
+let $slidercontent = $(".slidercontent");
 let $id;
 
-$bio.hide();
+let ref = firebase.database().ref();
+let users = ref.child("users");
+let uid = "kurkrUpfd4fg0fENIjVKfCNRfQV2";
+let userref = users.child(uid);
+let usermovies = userref.child("movies");
+
+const email = "danny@dyer.com";
+const pass = "password";
+const auth = firebase.auth();
+
+const promise = auth.signInWithEmailAndPassword(email, pass);
+
+$slidercontent.hide();
 
 console.log(url);
 
@@ -32,11 +44,17 @@ $.get(url, function(data) {
 
         if ($this.hasClass("selected")) {
           $this.removeClass("selected");
-          $bio.slideUp();
+          $slidercontent.slideUp();
         } else {
           $(".selected").removeClass("selected");
           $this.addClass("selected");
           detailedSearch(this.id);
+          $("html,body").animate(
+            {
+              scrollTop: $(".slidercontent").offset().top - $(this).height() / 5
+            },
+            500
+          );
         }
       })
 
@@ -78,33 +96,127 @@ function detailedSearch(id) {
     detailPlacement = (selectedRowNum - 1) * itemsPerRow - 1;
   }
 
-  $(".bio").empty();
+  $(".slidercontent").empty();
 
   $.get(moredetails, function(thismovie) {
     let results = $("<div>")
       .attr("class", "detailedlook")
       .attr("id", thismovie.imdbID)
       .append(
-        $("<img>")
-          .attr("src", thismovie.Poster)
-          .attr("class", "detailedposter")
+        $("<div>")
+          .attr("class", "detailedinfo")
+          .append(
+            $("<div>")
+              .attr("class", "movietitle")
+              .html(thismovie.Title)
+          )
+          .append(
+            $("<div>")
+              .attr("class", "moviesubinfo")
+              .append(
+                $("<div>")
+                  .attr("class", "movieyear")
+                  .html(thismovie.Year + " ")
+              )
+              .append(
+                $("<div>")
+                  .attr("class", "movierated")
+                  .html(thismovie.Rated)
+              )
+              .append(
+                $("<div>")
+                  .html(" " + thismovie.Runtime)
+                  .attr("class", "movieruntime")
+              )
+              .append(
+                $("<div>")
+                  .attr("class", "moviegenre")
+                  .html(thismovie.Genre)
+              )
+          )
+          .append(
+            $("<div>")
+              .attr("class", "moviedescription")
+              .html(thismovie.Plot)
+          )
+          .append(
+            $("<div>")
+              .attr("class", "movieawards")
+              .html(thismovie.Awards)
+          )
+          .append(
+            $("<div>")
+              .attr("class", "movieratings")
+              .html(thismovie.Ratings[0].Value)
+          )
       )
-      .appendTo($(".bio"));
+      .append(
+        $("<div>")
+          .attr("class", "selectbuttons")
+          .append(
+            $("<input>")
+              .attr("type", "button")
+              .attr("id", "request")
+              .attr("value", "Request")
+          )
+          .append(
+            $("<input>")
+              .attr("type", "button")
+              .attr("id", "bookmark")
+              .attr("value", "Bookmark")
+          )
+      )
+      .append(
+        $("<div>")
+          .attr("class", "detailedposter")
+          .append($("<img>").attr("src", thismovie.Poster))
+      )
+      .appendTo($(".slidercontent"));
   });
-  $(".bio")
+  $(".slidercontent")
     .insertAfter($(".movies").eq(detailPlacement))
     .slideDown();
 }
 
-$(window).resize(function() {
-  $(".bio").hide(); // hide Bio so it doesn't interfere with the flex layout during resize
-  clearTimeout(window.resizedFinished);
+class MovieRequest {
+  constructor(name, datetime) {
+    this.name = $(".detailedinfo")
+      .children(".movietitle")
+      .text();
+    this.datetime = Date.now();
+  }
+}
 
-  window.resizedFinished = setTimeout(function() {
-    $(".detailedlook").on(function() {
-      let tempid = $(this).attr(id);
-      console.log(tempid);
-    });
-    detailedSearch(tempid); // show Bio again now that resize has stabilised
-  }, 250);
+$(window).resize(function() {
+  if ($(".detailedlook")[0]) {
+    console.log($);
+    let tempid = $(".detailedlook").prop("id");
+    $(".slidercontent").hide(); // hide slider so it doesn't interfere with the flex layout during resize
+    clearTimeout(window.resizedFinished);
+
+    window.resizedFinished = setTimeout(function() {
+      detailedSearch(tempid);
+      $("html,body").animate(
+        {
+          scrollTop: $(".slidercontent").offset().top - $(this).height() / 5
+        },
+        500
+      ); // show slider again now that resize has stabilised
+    }, 250);
+  }
 });
+
+console.log();
+
+$(document).on("click", "#request", function() {
+  requestedMovie = new MovieRequest();
+  movieid = $(".detailedlook").prop("id");
+  moviedirectory = usermovies.child(movieid);
+  moviedirectory.set(requestedMovie);
+});
+
+/* $(function() {
+  $(window).on("scroll", function() {
+    $(".detailedposter").css("margin-top", 80 + $(window).scrollTop() * -0.3);
+  });
+}); */
